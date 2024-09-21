5cab5f30be24677147a9faa07c670cf28afeec6d
MeltedButter77 <meltedbuttergaming@gmail.com>
2024-09-21 22:52:34 +0930

    removed print line

--- channels.py
+++ channels.py
@@ -15,7 +15,6 @@ class Channels(commands.Cog):
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        print(member, before, after)
         guild = member.guild
         if after.channel:
             # Creates Temp Channel
