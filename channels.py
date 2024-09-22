3c81df1dd224c8da47d1dabcd43c34f80e11eb2d
Jack-Schultz <meltedbuttergaming@gmail.com>
2024-09-22 19:45:20 +0930

    Added channel selector menu

--- channels.py
+++ channels.py
@@ -12,13 +12,50 @@ with open('config.json') as config_file:
 sql_connection = sqlite3.connect('my_database.db')
 sql_cursor = sql_connection.cursor()
 sql_cursor.execute("CREATE TABLE IF NOT EXISTS temp_channels (guild_id INTEGER, channel_id INTEGER, number INTEGER)")
-sql_cursor.execute("CREATE TABLE IF NOT EXISTS temp_channel_hubs (guild_id INTEGER, channel_id INTEGER, number INTEGER)")
+sql_cursor.execute("CREATE TABLE IF NOT EXISTS temp_channel_hubs (guild_id INTEGER, channel_id INTEGER)")
 sql_connection.commit()
+
+class CreatorSelectView(View):
+    def __init__(self, channels):
+        super().__init__()
+        self.add_item(CreatorSelectMenu(channels))
+
+class CreatorSelectMenu(Select):
+    def __init__(self, channels):
+
+        options = []
+        for i, channel in enumerate(channels):
+            options.append(discord.SelectOption(label=f"#{channel.name} ({channel.id})", value=channel.id, emoji="🔧"))
+
+        super().__init__(placeholder="Choose a channel creator", options=options, min_values=1, max_values=1)
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"You selected: {self.values[0]}", ephemeral=True)
 
 
 class Channels(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @discord.app_commands.command()
+    async def setup_channel_creator(self, interaction: discord.Interaction):
+
+        query = 'SELECT channel_id FROM temp_channel_hubs WHERE guild_id = ?'
+        sql_cursor.execute(query, (interaction.guild.id,))
+        rows = sql_cursor.fetchall()
+
+        channels = []
+        for row in rows:
+            channels.append(self.bot.get_channel(row[0]))
+        print(channels)
+
+        # Creating an embed
+        embed = discord.Embed(title="Channel Hub Setup", description="Choose an option to set the channel hub.", color=0x00ff00)
+        embed.add_field(name="Options", value="You can choose one of the options from the dropdown below.", inline=False)
+
+        # Sending a message with the dropdown menu and embed
+        view = CreatorSelectView(channels)  # This creates the view with the dropdown
+        await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
 
     @discord.app_commands.command()
     async def set_channel_hub(self, interaction: discord.Interaction, channel: discord.VoiceChannel):
@@ -27,7 +64,7 @@ class Channels(commands.Cog):
         channel_hub_ids = [row[0] for row in sql_cursor.fetchall()]
 
         if channel.id in channel_hub_ids:
-            return await interaction.response.send_message("That channel is already a Hub", ephemeral=True)
+            return await interaction.response.send_message(f"<#{channel.id}> is already a Hub", ephemeral=True)
 
         query = 'INSERT INTO temp_channel_hubs (guild_id, channel_id, number) VALUES (?, ?, ?)'
         sql_cursor.execute(query, (interaction.guild.id, channel.id, 1))
