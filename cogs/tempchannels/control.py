a5abc53aae8ae7d4047cb67bb9359dca3e95194e
MeltedButter77 <meltedbuttergaming@gmail.com>
2024-10-02 18:13:05 +0930

    Added foundation for adding people to your channel

--- cogs/tempchannels/control.py
+++ cogs/tempchannels/control.py
@@ -1,9 +1,11 @@
 import json
 import os
+import time
+
 import discord
 from discord import app_commands
 from discord.ext import commands
-from discord.ui import View
+import discord.ui
 import databasecontrol
 import error_handling
 from enum import Enum
@@ -21,6 +23,95 @@ class ChannelState(Enum):
     HIDDEN = 2
 
 
+async def create_followup_menu(bot: commands.Bot, channel: discord.TextChannel, channel_state):
+    view = CreateFollowupView(
+        bot=bot,
+        channel=channel,
+        channel_state=channel_state,
+    )
+
+    # Get members that can view and connect
+    can_view_channel = []
+    can_connect = []
+
+    # Iterate through all members in the guild
+    for member in channel.guild.members:
+        # Check if the member can view the channel
+        if channel.permissions_for(member).view_channel:
+            can_view_channel.append(member)
+
+        # Check if the member can connect (for voice channels)
+        if channel.type == discord.ChannelType.voice and channel.permissions_for(member).connect:
+            can_connect.append(member)
+
+    embed = discord.Embed(
+        title=f"{channel.name}",
+        description = f"",
+        color=0x00ff00,
+    )
+    embed.add_field(
+        name="Users who can join",
+        value=f"{', '.join([member.mention for member in can_connect])}",
+        inline=True
+    )
+
+    text = ""
+    if channel_state == ChannelState.PUBLIC:
+        text = "Public"
+    elif channel_state == ChannelState.LOCKED:
+        text = "Locked"
+    elif channel_state == ChannelState.HIDDEN:
+        text = "Hidden"
+
+    return view, embed, text
+
+
+class CreateFollowupView(discord.ui.View):
+    """A view containing buttons and menus for managing channel creators."""
+
+    def __init__(self, bot: commands.Bot, channel, channel_state):
+        super().__init__(timeout=None)
+        self.bot = bot
+        self.channel = channel
+        self.channel_state = channel_state
+
+        button = discord.ui.Button(
+            label="button",
+            style=discord.ButtonStyle.primary
+        )
+        button.callback = self.button_callback
+
+        self.add_item(PermissionSelectMenu(bot=bot, channel=channel, channel_state=channel_state))
+
+        self.add_item(button)
+
+    async def button_callback(self, interaction: discord.Interaction):
+        print("clicked followup button")
+
+
+class PermissionSelectMenu(discord.ui.MentionableSelect):
+    """A dropdown menu for selecting a users or roles to give view and connect permissions to."""
+
+    def __init__(self,
+                bot: commands.Bot,
+                channel: discord.TextChannel,
+                channel_state = None,
+            ):
+        self.bot = bot
+        self.channel = channel
+        self.channel_state = channel_state
+
+        super().__init__(
+            placeholder="Select a user or role",
+            min_values=1,
+            max_values=25
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        """Handle the selection of a channel creator."""
+        pass
+
+
 async def create_control_menu(bot: commands.Bot, database: databasecontrol.Database, channel: discord.TextChannel, last_followup_message_id=None):
     view = CreateControlView(
         database=database,
@@ -31,7 +122,7 @@ async def create_control_menu(bot: commands.Bot, database: databasecontrol.Datab
 
     embed = discord.Embed(
         title="Manage your Temporary Channel",
-        description="",
+        description=f"Use the buttons below to manage your temporary channel.",
         color=0x00ff00
     )
     # embed.add_field(
@@ -42,7 +133,7 @@ async def create_control_menu(bot: commands.Bot, database: databasecontrol.Datab
     return view, embed
 
 
-class CreateControlView(View):
+class CreateControlView(discord.ui.View):
     """A view containing buttons and menus for managing channel creators."""
 
     def __init__(self, database, bot: commands.Bot, channel, last_followup_message_id):
@@ -105,28 +196,31 @@ class CreateControlView(View):
 
         if new_state == ChannelState.PUBLIC:
             permissions = {'connect': True, 'view_channel': True}
-            text = "Your channel is now public."
         elif new_state == ChannelState.LOCKED:
             permissions = {'connect': False, 'view_channel': True}
-            text = "Your channel is now locked."
         elif new_state == ChannelState.HIDDEN:
             permissions = {'view_channel': False}
-            text = "Your channel is now hidden."
         else:
             await error_handling.handle_global_error("Unexpected channel state")
             return
 
+        # set permissions and channel_state in database
+        connected_users = [member for member in interaction.channel.members if not member.bot]
+        for user in connected_users:
+            await interaction.channel.set_permissions(user, connect=True, view_channel=True)
         await interaction.channel.set_permissions(interaction.guild.default_role, **permissions)
         self.database.update_channel_state(interaction.channel.guild.id, interaction.channel.id, new_state.value)
 
+        # Create the followup message and menu
+        view, embed, text = await create_followup_menu(self.bot, interaction.channel, new_state)
         if self.last_followup_message_id:
             try:
                 followup_message = await interaction.channel.fetch_message(self.last_followup_message_id)
-                await followup_message.edit(content=text)
+                await followup_message.edit(content=text, view=view, embed=embed)
             except discord.NotFound:
-                followup_message = await interaction.followup.send(text)
+                followup_message = await interaction.followup.send(content=text, view=view, embed=embed)
         else:
-            followup_message = await interaction.followup.send(text)
+            followup_message = await interaction.followup.send(content=text, view=view, embed=embed)
 
         view, embed = await create_control_menu(self.bot, self.database, interaction.channel, followup_message.id)
         await interaction.message.edit(embed=embed, view=view)
