c317fac62e3a923099f9445945ffda89a15a3286
MeltedButter77 <meltedbuttergaming@gmail.com>
2025-03-05 21:14:22 +1030

    now prints channel members because its cool

--- cogs/tempchannels/manage.py
+++ cogs/tempchannels/manage.py
@@ -495,6 +495,7 @@ class TempChannelsCog(commands.Cog):
 
         for channel_id in channels:
             channel = self.bot.get_channel(channel_id)
+            print(channel.members)
             # Remove invalid and empty
             if not channel:
                 self.database.delete_temp_channel(channel_id)
